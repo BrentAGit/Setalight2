a4895f1baaf8a265c3f5739d5522a8550801a454
Kasper Haepers <kasper.haepers@outlook.be>
2020-04-21 14:35:58 +0200

    test?

---
+++ src/main/java/be/thomasmore/setalight/SetalightApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.setalight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetalightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SetalightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/thomasmore/setalight/SetalightApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.setalight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetalightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
