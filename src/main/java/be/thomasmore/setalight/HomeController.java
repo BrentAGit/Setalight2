f6f1fdc9dab222815d2b3c7e7f73108d1e162142
Kasper Haepers <kasper.haepers@outlook.be>
2020-04-22 16:11:54 +0200

    edited the basic homepage and added thymeleaf

--- src/main/java/be/thomasmore/setalight/HomeController.java
+++ src/main/java/be/thomasmore/setalight/HomeController.java
@@ -1,13 +1,17 @@
 package be.thomasmore.setalight;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    private String application = "Setalight";
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("application", this.application);
         return "home";
     }
 
