9008099ddd069bbc7b8e9460939a9a27611dcc97
Kasper Haepers <kasper.haepers@outlook.be>
2020-04-25 15:09:49 +0200

    Users are able to login and logout

--- src/main/java/be/thomasmore/setalight/SecurityConfiguration.java
+++ src/main/java/be/thomasmore/setalight/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package be.thomasmore.setalight;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,11 +15,14 @@ import javax.sql.DataSource;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Autowired private DataSource dataSource;
+    @Autowired
+    private DataSource dataSource;
 
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/admin/**").authenticated().anyRequest().permitAll().and().formLogin();
         http.csrf().ignoringAntMatchers("/h2-console/**").and().headers().frameOptions().sameOrigin();
+        http.formLogin().loginPage("/user/login").defaultSuccessUrl("/", true);
+        http.logout().logoutUrl("/user/logout").logoutSuccessUrl("/");
     }
 
     @Autowired
@@ -32,6 +36,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean()
+            throws Exception {
+        return super.authenticationManagerBean();
     }
 
 }
--- src/main/java/be/thomasmore/setalight/controllers/HomeController.java
+++ src/main/java/be/thomasmore/setalight/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package be.thomasmore.setalight.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,20 +12,24 @@ import java.security.Principal;
 @Controller
 public class HomeController {
 
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
     private String application = "Setalight";
 
     @GetMapping("/")
     public String home(Principal principal, Model model) {
-        String loggedInName = principal!=null ? principal.getName() : "nobody";
-        System.out.println(loggedInName);
+        String loggedInName = principal != null ? principal.getName() : "nobody";
+        logger.info(String.format("logged in: %s",
+                loggedInName));
         model.addAttribute("application", this.application);
         return "home";
     }
 
     @GetMapping("/admin/test")
     public String testPage(Principal principal) {
-        String loggedInName = principal!=null ? principal.getName() : "nobody";
-        System.out.println(loggedInName);
+        String loggedInName = principal != null ? principal.getName() : "nobody";
+        logger.info(String.format("logged in: %s",
+                loggedInName));
         return "/admin/test";
     }
 
