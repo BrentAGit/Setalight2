42b538afc0f86629b96a246f0666e77d4d94c261
Kasper Haepers <r0803085@student.thomasmore.be>
2020-05-02 14:11:09 +0200

    added testPage for productiehuis

--- src/main/java/be/thomasmore/setalight/controllers/ProductiehuisController.java
+++ src/main/java/be/thomasmore/setalight/controllers/ProductiehuisController.java
@@ -1,7 +1,15 @@
 package be.thomasmore.setalight.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ProductiehuisController {
+
+    @GetMapping("/page/{ID}")
+    public String profilePage(@PathVariable int ID, Model model) {
+        return ""
+    }
 }
