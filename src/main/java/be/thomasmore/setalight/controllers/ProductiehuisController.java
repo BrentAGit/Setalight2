61db9a134072cd1f34a6aa3db96a6129a7a9696a
Kasper Haepers <r0803085@student.thomasmore.be>
2020-05-02 14:14:09 +0200

    added model for productiehuis to test

--- src/main/java/be/thomasmore/setalight/controllers/ProductiehuisController.java
+++ src/main/java/be/thomasmore/setalight/controllers/ProductiehuisController.java
@@ -1,15 +1,26 @@
 package be.thomasmore.setalight.controllers;
 
+import be.thomasmore.setalight.models.User;
+import be.thomasmore.setalight.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.Optional;
+
 @Controller
 public class ProductiehuisController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/page/{ID}")
     public String profilePage(@PathVariable int ID, Model model) {
-        return ""
+        Optional<User> userFromDb = userRepository.findById(ID);
+        User user = userFromDb.get();
+        model.addAttribute("user", user);
+        return "/productiehuis/TEST";
     }
 }
