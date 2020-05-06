d044a4241a5f722cff5105ae5ca094401aea1b22
BrentAGit <r0787709@student.thomasmore.be>
2020-05-06 22:14:49 +0200

    Added GetMapping for verify

--- src/main/java/be/thomasmore/setalight/controllers/AdminController.java
+++ src/main/java/be/thomasmore/setalight/controllers/AdminController.java
@@ -14,10 +14,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/admin")
@@ -79,6 +78,13 @@ public class AdminController {
     public String verifyproductiehuis(Model model) {
         model.addAttribute("productiehuizen", userRepository.findUserByRoleAndVerified("PRODUCTIEHUIS", false));
         return "/admin/verifyproductiehuis";
+    }
+
+    @GetMapping({"/verify/{userId}"})
+    public String verify(@PathVariable int userId, Model model) {
+        Optional<User> userFromDb = userRepository.findById(userId);
+        model.addAttribute("user", userFromDb.get());
+        return "admin/verify";
     }
 
     @GetMapping({"/testAdmin"})
