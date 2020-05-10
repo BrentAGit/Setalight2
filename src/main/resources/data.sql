c295eb95e8becc25a2f73bcd65b5aa35ba394667
Kasper Haepers <r0803085@student.thomasmore.be>
2020-05-10 20:09:07 +0200

    added the events to profilepage.html in HTML and edited standard USER<->EVENT link to work easier with user marie

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,5 @@ insert into event(ID,AANTALDEELNEMERS,ADRES,CONTROLE,DATUM,DESCRIPTION,END_TIME,
     VALUES(0,50,'Zandpoortvest 60, 2800 Mechelen',true,'2020-05-17','Test description','15:00','testevent','19:00');
 insert into EVENT_USERS(EVENT_ID, USERS_ID)
     values(0, 0);
+insert into EVENT_USERS(EVENT_ID, USERS_ID)
+    values(0, 1);
