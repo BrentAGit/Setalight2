70f77f8800719ed171c1179a7ee4a25c700ac9f3
DS1705 <r0790860@student.thomasmore.be>
2020-05-13 13:51:12 +0200

    error's gefixt en toegevoegd dat sql queries weergegeven worden in console

--- src/main/java/be/thomasmore/setalight/models/User.java
+++ src/main/java/be/thomasmore/setalight/models/User.java
@@ -21,7 +21,7 @@ public class User {
     private String haircolor;
     private String fullpicture;
     private String physicalAtributes;
-    private String rijksregisternummer;
+    private String nationalInsuranceNumber;
     @ManyToMany
     private Collection<User> Frends;
     @ManyToMany
@@ -70,5 +70,85 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getProfilepicture() {
+        return profilepicture;
+    }
+
+    public void setProfilepicture(String profilepicture) {
+        this.profilepicture = profilepicture;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public String getHaircolor() {
+        return haircolor;
+    }
+
+    public void setHaircolor(String haircolor) {
+        this.haircolor = haircolor;
+    }
+
+    public String getFullpicture() {
+        return fullpicture;
+    }
+
+    public void setFullpicture(String fullpicture) {
+        this.fullpicture = fullpicture;
+    }
+
+    public String getPhysicalAtributes() {
+        return physicalAtributes;
+    }
+
+    public void setPhysicalAtributes(String physicalAtributes) {
+        this.physicalAtributes = physicalAtributes;
+    }
+
+    public String getNationalInsuranceNumber() {
+        return nationalInsuranceNumber;
+    }
+
+    public void setNationalInsuranceNumber(String nationalInsuranceNumber) {
+        this.nationalInsuranceNumber = nationalInsuranceNumber;
+    }
+
+    public Collection<User> getFrends() {
+        return Frends;
+    }
+
+    public void setFrends(Collection<User> frends) {
+        Frends = frends;
+    }
+
+    public Collection<Event> getErvaring() {
+        return ervaring;
+    }
+
+    public void setErvaring(Collection<Event> ervaring) {
+        this.ervaring = ervaring;
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-INSERT INTO USER (ID, USERNAME, PASSWORD, ROLE, VERIFIED)
-    VALUES (nextval('USER_SEQ'), 'admin', '$2a$10$9MIX8kYPkuB7uE/H5nHF8.KG6.YdjBA/voOnjSZnZDxLXL/2BIerS', 'ADMIN', false );--paswoord:admin
-INSERT INTO USER (ID, USERNAME, PASSWORD, ROLE,VERIFIED)
-    VALUES (nextval('USER_SEQ'), 'marie', '$2a$10$pQnldtWyD5CpMBjWJycMW.jmEOY2zKqjSkCo5V2vKqvW44ORrdpAa', 'USER', false);--paswoord:password
-INSERT INTO user (ID, USERNAME, PASSWORD, ROLE,VERIFIED)
-    VALUES (nextval('USER_SEQ'), 'testHuis', '$2a$10$pQnldtWyD5CpMBjWJycMW.jmEOY2zKqjSkCo5V2vKqvW44ORrdpAa', 'PRODUCTIEHUIS', false);--paswoord:password
+INSERT INTO USER (ID, USERNAME, PASSWORD, ROLE, VERIFIED, "LENGTH")
+    VALUES (nextval('USER_SEQ'), 'admin', '$2a$10$9MIX8kYPkuB7uE/H5nHF8.KG6.YdjBA/voOnjSZnZDxLXL/2BIerS', 'ADMIN', false,0 );--paswoord:admin
+INSERT INTO USER (ID, USERNAME, PASSWORD, ROLE, VERIFIED, "LENGTH")
+    VALUES (nextval('USER_SEQ'), 'marie', '$2a$10$pQnldtWyD5CpMBjWJycMW.jmEOY2zKqjSkCo5V2vKqvW44ORrdpAa', 'USER', false,0);--paswoord:password
+INSERT INTO user (ID, USERNAME, PASSWORD, ROLE, VERIFIED, "LENGTH")
+    VALUES (nextval('USER_SEQ'), 'testHuis', '$2a$10$pQnldtWyD5CpMBjWJycMW.jmEOY2zKqjSkCo5V2vKqvW44ORrdpAa', 'PRODUCTIEHUIS', false,0);--paswoord:password
 
 insert into event(ID,AANTALDEELNEMERS,ADRES,CONTROLE,DATUM,DESCRIPTION,END_TIME,"NAME",START_TIME)
     VALUES(0,50,'Zandpoortvest 60, 2800 Mechelen',true,'2020-05-17','Test description','15:00','testevent','19:00');
