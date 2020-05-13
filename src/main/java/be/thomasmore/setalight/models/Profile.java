941c78f793674cc633796ed9bc6171148a2b0813
ArneBril1997 <r0784247@student.thomasmore.com>
2020-05-14 01:58:30 +0200

    profile class attributes methods

--- src/main/java/be/thomasmore/setalight/models/Profile.java
+++ src/main/java/be/thomasmore/setalight/models/Profile.java
@@ -18,4 +18,43 @@ public class Profile {
     @OneToOne
     private User userId;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getHeight() {
+        return Height;
+    }
+
+    public void setHeight(String height) {
+        Height = height;
+    }
+
+    public String getHaircolor() {
+        return haircolor;
+    }
+
+    public void setHaircolor(String haircolor) {
+        this.haircolor = haircolor;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
 }
