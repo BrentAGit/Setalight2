cb556a2ec08ae7f7d72eda8e7b382cc958c1feab
ArneBril1997 <r0784247@student.thomasmore.com>
2020-05-14 01:55:16 +0200

    profile class attributes

--- src/main/java/be/thomasmore/setalight/models/Profile.java
+++ src/main/java/be/thomasmore/setalight/models/Profile.java
@@ -1,7 +1,21 @@
 package be.thomasmore.setalight.models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.util.Date;
 
 @Entity
 public class Profile {
+
+    @GeneratedValue
+    @Id
+    private Integer id;
+    private Date birthdate;
+    private String Height;
+    private String haircolor;
+    @OneToOne
+    private User userId;
+
 }
