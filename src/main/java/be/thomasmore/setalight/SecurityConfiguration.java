68b4e7b5d9fa261121f874b080b9b679fd4497ea
Kasper Haepers <r0803085@student.thomasmore.be>
2020-05-13 19:40:20 +0200

    fixed register error when want to register you have to be loged in first this is now fixed

--- src/main/java/be/thomasmore/setalight/SecurityConfiguration.java
+++ src/main/java/be/thomasmore/setalight/SecurityConfiguration.java
@@ -20,7 +20,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/user/**").authenticated()
+                .antMatchers("/user/register").permitAll()
+//                .antMatchers("/user/**").authenticated()
                 .antMatchers("/admin/register").permitAll()
                 .antMatchers("/event/events/**").hasAuthority("USER")
                 .antMatchers("/event/event", "/event/events", "/event/edit-event/**").hasAnyAuthority("ADMIN", "PRODUCTIEHUIS")
