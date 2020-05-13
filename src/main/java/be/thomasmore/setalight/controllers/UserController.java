ae6a654a4f149ad290cddbd9ff1306d0a874cda2
DS1705 <r0790860@student.thomasmore.be>
2020-05-13 23:17:00 +0200

    edited UserController with extra info but there is an error

--- src/main/java/be/thomasmore/setalight/controllers/UserController.java
+++ src/main/java/be/thomasmore/setalight/controllers/UserController.java
@@ -5,6 +5,8 @@ import be.thomasmore.setalight.repositories.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -15,7 +17,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 import java.util.Optional;
 
 @Controller
@@ -33,6 +39,9 @@ public class UserController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Value("${upload.images.dir}")
+    private String uploadImagesDirString;
+
     @GetMapping("/register")
     public String registerUser(Model model) {
         return "/user/register";
@@ -41,13 +50,33 @@ public class UserController {
     @PostMapping("/register")
     public String registered(@RequestParam String username,
                              @RequestParam String password,
+                             @RequestParam @DateTimeFormat(pattern = "yyyy-mm-dd") Date birthdate,
+                             @RequestParam String email,
+                             @RequestParam String haircolor,
+                             @RequestParam MultipartFile profilepicture,
+                             @RequestParam MultipartFile fullpicture,
+                             @RequestParam Double length ,
                              Model model) {
-        logger.info(String.format("username= %s -- password= %s\n",
+        logger.info(String.format("username= %s -- password= %s -- birthdate\n",
                 username, password));
         User user = new User();
         user.setUsername(username);
         user.setPassword(passwordEncoder.encode(password));
         user.setRole("USER");
+        user.setBirthdate(birthdate);
+        String profilePictureName = profilepicture.getOriginalFilename();
+        if(!profilePictureName.equals(user.getProfilepicture())){
+            File imageFileDir= new File(uploadImagesDirString);
+            if(!imageFileDir.exists()){
+                imageFileDir.mkdir();
+            }
+            File imageFile= new File(uploadImagesDirString,profilePictureName);
+                try {
+                    profilepicture.transferTo(imageFile);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         userRepository.save(user);
         autologin(username, password);
         return "redirect:/";
