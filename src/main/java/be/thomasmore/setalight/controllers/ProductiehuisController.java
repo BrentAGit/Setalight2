dee9606b4a07909616b06a3620773f8f18d30e38
BrentAGit <r0787709@student.thomasmore.be>
2020-05-14 11:23:32 +0200

    Added Get- and Postmapping for ProductiehuisController

--- src/main/java/be/thomasmore/setalight/controllers/ProductiehuisController.java
+++ src/main/java/be/thomasmore/setalight/controllers/ProductiehuisController.java
@@ -2,12 +2,19 @@ package be.thomasmore.setalight.controllers;
 
 import be.thomasmore.setalight.models.User;
 import be.thomasmore.setalight.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -15,8 +22,16 @@ import java.util.Optional;
 @RequestMapping("/productiehuis")
 public class ProductiehuisController {
 
+    private Logger logger = LoggerFactory.getLogger(ProductiehuisController.class);
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @GetMapping("/page/{ID}")
     public String profilePage(@PathVariable int ID, Model model) {
@@ -24,5 +39,38 @@ public class ProductiehuisController {
         User user = userFromDb.get();
         model.addAttribute("user", user);
         return "/productiehuis/TEST";
+    }
+
+    @GetMapping("/registerProductiehuis")
+    public String registerProductiehuis(Model model) {
+        return "/productiehuis/registerProductiehuis";
+    }
+
+    @PostMapping("/registerProductiehuis")
+    public String registeredProductiehuis(@RequestParam String username,
+                             @RequestParam String password,
+                             Model model) {
+        logger.info(String.format("username= %s -- password= %s\n",
+                username, password));
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setRole("PRODUCTIEHUIS");
+        user.setVerified(false);
+        userRepository.save(user);
+        autologin(username, password);
+        return "redirect:/";
+    }
+
+    private void autologin(String userName, String password) {
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userName, password);
+        try {
+            Authentication auth = authenticationManager.authenticate(token);
+            logger.info("authentication: " + auth.isAuthenticated());
+            SecurityContext sc = SecurityContextHolder.getContext();
+            sc.setAuthentication(auth);
+        } catch (AuthenticationException e) {
+            e.printStackTrace();
+        }
     }
 }
