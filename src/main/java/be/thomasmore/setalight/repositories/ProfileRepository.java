4462abc61aaa5d3ad1a563831eb930413ba3ca93
Kasper Haepers <r0803085@student.thomasmore.be>
2020-05-15 10:54:58 +0200

    added in the ProfileRepository the option to find profiles by user

--- src/main/java/be/thomasmore/setalight/repositories/ProfileRepository.java
+++ src/main/java/be/thomasmore/setalight/repositories/ProfileRepository.java
@@ -1,7 +1,12 @@
 package be.thomasmore.setalight.repositories;
 
 import be.thomasmore.setalight.models.Profile;
+import be.thomasmore.setalight.models.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ProfileRepository extends CrudRepository<Profile, Integer> {
+
+    Optional<Profile> findByUserId(User user);
 }
