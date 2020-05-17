619427dfb348576490aad46c68f8a8c592ca615c
Kasper Haepers <r0803085@student.thomasmore.be>
2020-05-17 20:27:33 +0200

    some eni meny little thing

--- src/main/java/be/thomasmore/setalight/models/Profile.java
+++ src/main/java/be/thomasmore/setalight/models/Profile.java
@@ -9,7 +9,7 @@ public class Profile {
 
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "profile_generator")
     @SequenceGenerator(name = "profile_generator", sequenceName = "profile_seq",
-            initialValue = 0, allocationSize = 1)
+            initialValue = 1, allocationSize = 1)
     @Id
     private Integer id;
     private Date birthdate;
