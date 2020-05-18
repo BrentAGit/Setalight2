7ad25db3f7b0a7401f99214bb250b312cd2f227d
DS1705 <r0790860@student.thomasmore.be>
2020-05-19 00:49:56 +0200

    ProductiehuisProfileRepository.java aangemaakt

---
+++ src/main/java/be/thomasmore/setalight/repositories/ProductiehuisProfileRepository.java
@@ -0,0 +1,8 @@
+package be.thomasmore.setalight.repositories;
+
+import be.thomasmore.setalight.models.ProductiehuisProfile;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductiehuisProfileRepository extends CrudRepository<ProductiehuisProfile,Integer> {
+
+}
