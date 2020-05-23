5f4220db29d2b68007a623040c641f7f8055d1db
Kasper Haepers <r0803085@student.thomasmore.be>
2020-05-23 14:43:59 +0200

    added the RewardRepository.java

---
+++ src/main/java/be/thomasmore/setalight/repositories/RewardRepository.java
@@ -0,0 +1,8 @@
+package be.thomasmore.setalight.repositories;
+
+import be.thomasmore.setalight.models.Reward;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RewardRepository extends CrudRepository<Reward, Integer> {
+
+}
