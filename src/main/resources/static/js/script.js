c02b0a831f29060f4208437d5a51ac94fe6fc504
Kasper Haepers <r0803085@student.thomasmore.be>
2020-05-23 15:56:12 +0200

    moved all the js to one file and started on checking if the user has enough points to buy a reward with js

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,39 @@
+function checkPassword(form) {
+    password1 = form.password.value;
+    password2 = form.rePassword.value;
+    username = form.username.value;
+    let noPassword = document.getElementById("noPassword");
+    let noConfirmPassword = document.getElementById("noConfirmPassword");
+    let noMatch = document.getElementById("noMatch");
+
+    if (password1 === '') {
+        noPassword.style.display = "inline-block";
+        noConfirmPassword.style.display = "none";
+        noMatch.style.display = "none";
+        return false;
+    } else if (password2 === '') {
+        noPassword.style.display = "none";
+        noConfirmPassword.style.display = "inline-block";
+        noMatch.style.display = "none";
+        return false;
+    } else if (password1 !== password2) {
+        noPassword.style.display = "none";
+        noConfirmPassword.style.display = "none";
+        noMatch.style.display = "inline-block";
+        return false;
+    } else if (username === "" && password1 === "" && password2 === "") {
+        return false;
+    } else {
+        noPassword.style.display = "none";
+        noConfirmPassword.style.display = "none";
+        noMatch.style.display = "none";
+        return true;
+    }
+}
+
+function checkPoints(form, Window) {
+    let userPoints = Window.getElementById("points");
+    let rewardPoints = Window.getElementById("reward-points");
+    console.log(userPoints);
+    console.log(rewardPoints);
+}
