9dcfce19c2b94a1263ff398ccf4f4251dbec279b
Kasper Haepers <r0803085@student.thomasmore.be>
2020-05-23 14:39:55 +0200

    added Reward class

---
+++ src/main/java/be/thomasmore/setalight/models/Reward.java
@@ -0,0 +1,39 @@
+package be.thomasmore.setalight.models;
+
+import javax.persistence.*;
+
+public class Reward {
+
+    @Id
+    private Integer id;
+    private String name;
+    private int points;
+
+    public Reward() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
